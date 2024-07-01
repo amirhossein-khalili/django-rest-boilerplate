31c789291d4fd5787a668c3d3725913a1111bb02
amirkhalili <amir1378khalili@gmail.com>
2024-07-01 12:59:28 +0330

    ADD auth token creator

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from rest_framework.authtoken import views as auth_token_views
 
 from . import views
 
 app_name = "accounts"
 urlpatterns = [
     path("register", views.UserRegister.as_view()),
+    path("auth-token", auth_token_views.obtain_auth_token),
 ]
