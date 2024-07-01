eb6ae6b16abc05dacaf8f690ea5ea2ada3103056
amirkhalili <amir1378khalili@gmail.com>
2024-07-01 11:58:31 +0330

    ENHANCE serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -7,21 +7,27 @@ def clean_email(value):
         raise serializers.ValidationError("Email can't contain 'admin'!!")
 
 
-class UserRegisterSerializer(serializers.Serializer):
-
-    username = serializers.CharField(required=True)
-    email = serializers.EmailField(required=True, validators=[clean_email])
-    password = serializers.CharField(required=True, write_only=True)
+class UserRegisterSerializer(serializers.ModelSerializer):
     repeat_password = serializers.CharField(required=True, write_only=True)
 
+    class Meta:
+        model = User
+        fields = ("username", "password", "email", "repeat_password")
+        extra_kwargs = {
+            "password": {"write_only": True},
+            "email": {"validators": [clean_email]},
+        }
+
+    def create(self, validated_data):
+        validated_data.pop("repeat_password")
+        user = User.objects.create_user(**validated_data)
+        return user
+
     def validate_username(self, value):
-        if value == "admin":
+        if value.lower() == "admin":
             raise serializers.ValidationError("Username can't be 'admin'!!")
-        user = User.objects.filter(username=value)
-        print(user)
         if User.objects.filter(username=value).exists():
             raise serializers.ValidationError("This username is already taken!!")
-
         return value
 
     def validate(self, data):
