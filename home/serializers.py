6477fce5d436712c0497671b7274786e956aa55d
amirkhalili <amir1378khalili@gmail.com>
2024-07-03 17:46:14 +0330

    ADD QuestionSerializer , AnswerSerializer

--- home/serializers.py
+++ home/serializers.py
@@ -1,8 +1,30 @@
 from rest_framework import serializers
 
+from .models import Answer, Person, Question
+
 
 class PersonSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     name = serializers.CharField()
     age = serializers.IntegerField()
-    # email = serializers.EmailField() 
+    # email = serializers.EmailField()
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    # user = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = Question
+        fields = "__all__"
+
+    # def create(self, validated_data):
+    #     request = self.context.get("request")
+    #     validated_data["user"] = request.user
+    #     return super().create(validated_data)
+
+
+class AnswerSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Answer
+        fields = "__all__"
