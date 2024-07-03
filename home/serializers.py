91df1d53659cfe216d2980a2ac224f293dbfd04a
amirkhalili <amir1378khalili@gmail.com>
2024-07-03 21:40:59 +0330

    Formate serializer

--- home/serializers.py
+++ home/serializers.py
@@ -18,9 +18,10 @@ class QuestionSerializer(serializers.ModelSerializer):
         model = Question
         fields = "__all__"
 
-    def get_answers (self , obj):
+    def get_answers(self, obj):
         result = obj.answers.all()
-        return  AnswerSerializer(instance=result , many=True).data
+        return AnswerSerializer(instance=result, many=True).data
+
     # def create(self, validated_data):
     #     request = self.context.get("request")
     #     validated_data["user"] = request.user
