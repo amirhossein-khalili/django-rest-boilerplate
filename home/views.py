80c9a652a0f99eb3269d48a97028874b0329ad3e
amirkhalili <amir1378khalili@gmail.com>
2024-07-03 19:56:56 +0330

    COMPLETE‌ crud answer , question

--- home/views.py
+++ home/views.py
@@ -61,40 +61,66 @@ class QuestionView(APIView):
     def patch(self, request, pk):
         try:
             question = Question.objects.get(pk=pk)
-            ser_data = QuestionSerializer(
-                instance=question, data=request.data, partial=True
-            )
-            if ser_data.is_valid():
-                ser_data.save()
-                return Response(ser_data.data, status=status.HTTP_200_OK)
-            return Response(ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
         except Question.DoesNotExist:
             return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+        ser_data = QuestionSerializer(
+            instance=question, data=request.data, partial=True
+        )
+        if ser_data.is_valid():
+            ser_data.save()
+            return Response(ser_data.data, status=status.HTTP_200_OK)
+        return Response(ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
         try:
             question = Question.objects.get(pk=pk)
-            question.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
         except Question.DoesNotExist:
             return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
 
+        question.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 class AnswerView(APIView):
-    def get(self, request):
-        answers = Answer.objects.all()
-        ser_data = AnswerSerializer(instance=answers, many=True).data
-        return Response(ser_data, status=status.HTTP_200_OK)
+    def get(self, request, pk=None):
+        if pk is not None:
+            try:
+                answer = Answer.objects.get(pk=pk)
+                ser_data = AnswerSerializer(instance=answer).data
+                return Response(ser_data, status=status.HTTP_200_OK)
+            except Answer.DoesNotExist:
+                return Response(
+                    {"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND
+                )
+        else:
+            answers = Answer.objects.all()
+            ser_data = AnswerSerializer(instance=answers, many=True).data
+            return Response(ser_data, status=status.HTTP_200_OK)
 
     def post(self, request):
+        request.data["user"] = request.user.id
         ser_data = AnswerSerializer(data=request.data)
         if ser_data.is_valid():
             ser_data.save()
             return Response(ser_data.data, status=status.HTTP_201_CREATED)
         return Response(ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    # def put(self, request, pk):
-    #     pass
+    def patch(self, request, pk):
+        try:
+            answer = Answer.objects.get(pk=pk)
+        except Answer.DoesNotExist:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+        ser_data = AnswerSerializer(instance=answer, data=request.data, partial=True)
+        if ser_data.is_valid():
+            ser_data.save()
+            return Response(ser_data.data, status=status.HTTP_200_OK)
+        return Response(ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    # def get(self, request, pk):
-    #     pass
+    def delete(self, request, pk):
+        try:
+            answer = Answer.objects.get(pk=pk)
+        except Answer.DoesNotExist:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+
+        answer.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
