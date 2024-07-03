ad68c4781fb062df353df58148b0c1ffc849c8db
amirkhalili <amir1378khalili@gmail.com>
2024-07-03 19:57:24 +0330

    ADD questions , answers pk items

--- home/urls.py
+++ home/urls.py
@@ -6,5 +6,7 @@ app_name = "home"
 urlpatterns = [
     path("", views.Home.as_view()),
     path("questions", views.QuestionView.as_view()),
+    path("questions/<int:pk>", views.QuestionView.as_view()),
     path("answers", views.AnswerView.as_view()),
+    path("answers/<int:pk>", views.AnswerView.as_view()),
 ]
