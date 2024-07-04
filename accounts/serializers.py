b15907749d694c281690f313de98f385dddad13a
amirkhalili <amir1378khalili@gmail.com>
2024-07-04 19:53:57 +0330

    change password field

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -39,6 +39,8 @@ class UserRegisterSerializer(serializers.ModelSerializer):
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
         fields = "__all__"
