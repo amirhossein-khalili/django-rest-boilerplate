295d20aca24910f8cc62b1f9c8035ecfba469179
amirkhalili <amir1378khalili@gmail.com>
2024-07-04 17:21:47 +0330

    REMOVE basic auth token

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path
-from rest_framework.authtoken import views as auth_token_views
 from rest_framework import routers
-
 
 from . import views
 
 app_name = "accounts"
 urlpatterns = [
     path("register/", views.UserRegister.as_view()),
-    path("auth-token/", auth_token_views.obtain_auth_token),
 ]
 
 
