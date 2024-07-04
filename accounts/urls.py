29b9bc0caf720f3d26c05ef23bfb87d4c20d2bf1
amirkhalili <amir1378khalili@gmail.com>
2024-07-04 14:44:48 +0330

    ADD router users

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 from rest_framework.authtoken import views as auth_token_views
+from rest_framework import routers
+
 
 from . import views
 
 app_name = "accounts"
 urlpatterns = [
-    path("register", views.UserRegister.as_view()),
-    path("auth-token", auth_token_views.obtain_auth_token),
+    path("register/", views.UserRegister.as_view()),
+    path("auth-token/", auth_token_views.obtain_auth_token),
 ]
+
+
+router = routers.SimpleRouter()
+router.register("user", views.UserViewSet)
+urlpatterns += router.urls
