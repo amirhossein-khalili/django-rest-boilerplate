2c5a84fdeb6c248eb6da4916bb19b238150aca9a
amirkhalili <amirkhalili.mni@gmail.com>
2025-03-13 20:19:43 +0330

    ADD user , otp models

--- accounts/models.py
+++ accounts/models.py
@@ -1,1 +1,44 @@
+import datetime
+
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.utils import timezone
+
+from .managers import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    phone = models.CharField(max_length=20, unique=True)
+    email = models.EmailField(blank=True, null=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "phone"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.phone
+
+
+class OTP(models.Model):
+    phone = models.CharField(max_length=20)
+    code = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expires_at = models.DateTimeField()
+    used = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if not self.pk and not self.expires_at:
+            self.expires_at = timezone.now() + datetime.timedelta(minutes=5)
+        super().save(*args, **kwargs)
+
+    def is_valid(self):
+        return not self.used and timezone.now() < self.expires_at
+
+    def __str__(self):
+        return f"OTP for {self.phone} (code: {self.code})"
