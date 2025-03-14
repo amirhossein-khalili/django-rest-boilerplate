0f955d9a1485d8d3b867a6faed2e10508539a279
amirkhalili <amirkhalili.mni@gmail.com>
2025-03-14 11:43:38 +0330

    FIX typo

--- accounts/services/abstracts/jwt_service.py
+++ accounts/services/abstracts/jwt_service.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 
 
-class wAbstractJWTService(ABC):
+class AbstractJWTService(ABC):
     @abstractmethod
     def generate_token(self, user) -> str:
         """
